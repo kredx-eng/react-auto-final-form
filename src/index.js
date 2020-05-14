155f0c6e6d6aeba6ebbfb9f98116a017b188c352
Aarshi <aarshi.anand@kredx.com>
2020-05-14 12:17:28 +0530

    Revert "Tsc conversion"
    This reverts commit faca303d9a26bb866ce086e78c7f383b85ae2916.

--- src/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var FormBuilder_1 = require("./FormBuilder/FormBuilder");
-exports["default"] = FormBuilder_1.FormBuilder;
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.register();
--- src/setupTests.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var enzyme_1 = require("enzyme");
-var enzyme_adapter_react_16_1 = require("enzyme-adapter-react-16");
-enzyme_1.configure({ adapter: new enzyme_adapter_react_16_1["default"]() });
